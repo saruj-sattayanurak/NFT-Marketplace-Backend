04fa720236bbcda50e7cc305a6ebb9b8e6cfde00
saruj <saruj@sarujs-MacBook-Pro.local>
2023-03-28 23:55:59 +0700

    GET by foundation id

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :artworks, only: [:show, :create, :index]
       get '/artworks/by_owner/:address', to: 'artworks#owner'
+      get '/artworks/by_foundation/:foundation_id', to: 'artworks#nft_by_foundation'
       resources :foundations, only: [:index]
     end
   end
